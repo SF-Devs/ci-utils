c757eacb4e62e13045eef849f6efd60af085790b
Marcelo Costa <marcelo.cost@marceloco-ltm.internal.salesforce.com>
2021-05-19 16:06:55 +1000

    implemented ciutils:generateTestClassNames

---
+++ src/commands/ciutils/generateTestClassNames.ts
@@ -0,0 +1,106 @@
+import { flags, SfdxCommand } from '@salesforce/command';
+import { Messages, SfdxError } from '@salesforce/core';
+import { AnyJson } from '@salesforce/ts-types';
+
+// Initialize Messages with the current plugin directory
+Messages.importMessagesDirectory(__dirname);
+
+// Load the specific messages for this file. Messages from @salesforce/command, @salesforce/core,
+// or any library that is using the messages framework can also be loaded this way.
+const messages = Messages.loadMessages('sfciutils', 'generateTestClassNames');
+
+export default class Org extends SfdxCommand {
+
+  public static description = messages.getMessage('commandDescription');
+
+  public static examples = [
+    `$ sfdx ciutils:generateTestClassNames --path
+  Class1,Class2,Class3
+  `
+  ];
+
+  public static args = [{name: 'file'}];
+
+  protected static flagsConfig = {
+    // flag with a value (-n, --name=VALUE)
+    path: flags.string({char: 'p', description: messages.getMessage('pathFlagDescription')})
+  };
+
+  // Comment this out if your command does not require an org username
+  protected static requiresUsername = false;
+
+  // Comment this out if your command does not support a hub org username
+  protected static supportsDevhubUsername = false;
+
+  // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
+  protected static requiresProject = false;
+
+  public async run(): Promise<AnyJson> {
+    // validate mandatory input
+    if (null == this.flags.path) {
+      throw new SfdxError('Path is mandatory');
+    }
+    const path = this.flags.path;
+    const fs = require('fs');
+    // Checks if folder exists
+    if (false === fs.existsSync(path)) {
+      throw new SfdxError('Path does not exist or invalid');
+    }
+    // get all classes on the folder
+    const files = fs.readdirSync(path).filter(file => {
+      return file.endsWith('.cls');
+    });
+    // checks if the files contains the @IsTest string
+    const testFiles = files.filter(file => {
+      const data = fs.readFileSync(path + '/' + file).toString().toLowerCase();
+      return (data.includes('@istest'));
+    });
+
+    // const name = this.flags.name || 'world';
+
+    // this.org is guaranteed because requiresUsername=true, as opposed to supportsUsername
+    // const conn = this.org.getConnection();
+    // const query = 'Select Name, TrialExpirationDate from Organization';
+
+    // The type we are querying for
+    // interface Organization {
+    //  Name: string;
+    //  TrialExpirationDate: string;
+    // }
+
+    // Query the org
+    // const result = await conn.query<Organization>(query);
+
+    // Organization will always return one result, but this is an example of throwing an error
+    // The output and --json will automatically be handled for you.
+    // if (!result.records || result.records.length <= 0) {
+    //  throw new SfdxError(messages.getMessage('errorNoOrgResults', [this.org.getOrgId()]));
+    // }
+
+    // Organization always only returns one result
+    // const orgName = result.records[0].Name;
+    // const trialExpirationDate = result.records[0].TrialExpirationDate;
+
+    // let outputString = `Hello ${name}! This is org: ${orgName}`;
+    // if (trialExpirationDate) {
+    //  const date = new Date(trialExpirationDate).toDateString();
+    // outputString = `${outputString} and I will be around until ${date}!`;
+    // }
+    const returnString = testFiles.join().replaceAll('.cls','');
+    this.ux.log(returnString);
+
+    // this.hubOrg is NOT guaranteed because supportsHubOrgUsername=true, as opposed to requiresHubOrgUsername.
+    // if (this.hubOrg) {
+    //  const hubOrgId = this.hubOrg.getOrgId();
+    //  this.ux.log(`My hub org id is: ${hubOrgId}`);
+    // }
+
+    // if (this.flags.force && this.args.file) {
+    //  this.ux.log(`You input --force and a file: ${this.args.file}`);
+    // }
+
+    // Return an object to be displayed with --json
+    // return { orgId: this.org.getOrgId(), outputString };
+    return testFiles;
+  }
+}
